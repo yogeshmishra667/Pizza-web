5d7de2fee1a397137a4323f34c73c652323b31e1
yogesh mishra <128018592+Yogesh-Bytive@users.noreply.github.com>
2024-07-28 15:30:29 +0530

    chore: Add initial project files and configurations

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./UI/Home"
+import Menu, {loader as menuLoader } from "./features/menu/Menu"
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+import AppLayout from './UI/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+
+export default function App() {
+  return <RouterProvider router = { router} />
+}
